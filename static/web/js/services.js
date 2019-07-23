fe25cb9f7d23d14d86b84e8db25a05fb95443870
Yann Savary <yann.savary@gmail.com>
2019-07-23 23:31:15 +0200

    Updated client to use api version 2.2

--- static/web/js/services.js
+++ static/web/js/services.js
@@ -102,7 +102,7 @@ angular.module('windmobile.services', [])
                 return +(Math.round(num + "e+3") + "e-3");
             },
 
-            apiUrl: "https://winds.mobi/api/2.1",
+            apiUrl: "https://winds.mobi/api/2.2",
             apiUserUrl: "https://winds.mobi/user"
         }
     }])
