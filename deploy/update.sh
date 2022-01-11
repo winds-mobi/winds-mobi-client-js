9fb02192e57062d4f5477553d5389bbbc0d64a51
Yann Savary <yann@winds.mobi>
2022-01-11 21:30:01 +0100

    Fixed MaxCDN script

--- deploy/update.sh
+++ deploy/update.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-su - winds -c "cd /home/winds/winds-mobi-client-js; git fetch; git pull"
-su - winds -c "python3 /home/winds/winds-mobi-client-js/deploy/maxcdn_purge_cache.py"
+if [[ $(id -u) -eq 0 ]] ; then echo "Please run as user 'winds'" ; exit 1 ; fi
+
+cd /home/winds/winds-mobi-client-js; git fetch; git pull
+python3 /home/winds/winds-mobi-client-js/deploy/maxcdn_purge_cache.py
