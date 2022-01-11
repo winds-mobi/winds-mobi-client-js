0812f7814c4ce6840aa4c063f094eacc7e08d071
Yann Savary <yann@winds.mobi>
2022-01-11 21:33:23 +0100

    Fixed typo

--- deploy/maxcdn_purge_cache.py
+++ deploy/maxcdn_purge_cache.py
@@ -1,7 +1,7 @@
 import os
 from maxcdn import MaxCDN
 
-api = MaxCDN("windsmobi", os.environ["MAXCDN_KEY"], os.environ["MAXCDN_SECRET]")
+api = MaxCDN("windsmobi", os.environ["MAXCDN_KEY"], os.environ["MAXCDN_SECRET"])
 
 # Purge All Cache
 api.delete("/zones/pull.json/470828/cache")
