dcde8321f9080ff5bd6e49efadd8bda709c0023c
Yann Savary <yann@winds.mobi>
2022-07-29 18:00:51 +0200

    Updated deployment files

--- deploy/maxcdn_purge_cache.py
+++
@@ -1,8 +0,0 @@
-import os
-from maxcdn import MaxCDN
-
-api = MaxCDN("windsmobi", os.environ["MAXCDN_KEY"], os.environ["MAXCDN_SECRET"])
-
-# Purge All Cache
-api.delete("/zones/pull.json/470828/cache")
-print("MaxCDN cache purged")
--- deploy/update.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-if [[ $(id -u) -eq 0 ]] ; then echo "Please run as user 'winds'" ; exit 1 ; fi
-
-cd /home/winds/winds-mobi-client-js; git fetch; git pull
-python3 /home/winds/winds-mobi-client-js/deploy/maxcdn_purge_cache.py
